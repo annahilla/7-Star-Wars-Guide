39a0c5c5ca253f51f5ba8d701ace3f3c49bf55d2
Anna <annahilla21@gmail.com>
2024-11-20 12:55:36 +0100

    Implement protected routes when not logged in

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,39 @@ import MainLayout from "./layouts/MainLayout";
 import StarshipDetails from "./pages/StarshipDetailsPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import AuthListener from "./utils/AuthListener";
 
 function App() {
+  const isLoggedIn = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+  
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
-        <Route path="/starships" element={<StarshipsPage />} />
-        <Route path="/starships/:name" element={<StarshipDetails />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
         <Route index element={<HomePage />} />
+        <Route element={<ProtectedRoute canActivate={isLoggedIn} redirectPath="/login" />}>
+          <Route path="/starships" element={<StarshipsPage />} />
+          <Route path="/starships/:name" element={<StarshipDetails />} />
+        </Route>
+        <Route element={<ProtectedRoute canActivate={!isLoggedIn} redirectPath="/" />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Route>
+        
       </Route>
     )
   );
 
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <AuthListener />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/utils/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+interface ProtectedRouteProps {
+    canActivate: boolean,
+    redirectPath: string
+}
+
+const ProtectedRoute = ({
+    canActivate,
+    redirectPath = "/"
+} : ProtectedRouteProps) => {
+    if(!canActivate) {
+        return <Navigate to={redirectPath} replace />
+    }
+
+    return <Outlet />
+}
+
+export default ProtectedRoute;
